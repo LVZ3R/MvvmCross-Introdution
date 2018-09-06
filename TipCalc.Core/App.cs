10dc65729390e386de8b61565ed20d41272fb5bc
Vladislav Antonov <lvz3rz@gmail.com>
2018-09-06 14:33:48 +0300

    Read description
    Створено проект для ознайомлення з можливостями MvvmCross.

---
+++ TipCalc.Core/App.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MvvmCross;
+using MvvmCross.ViewModels;
+using TipCalc.Core.ViewModels;
+using TipCalc.Core.Services;
+
+namespace TipCalc.Core
+{
+    class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            Mvx.RegisterType<ICalculationService, CalculationService>();
+
+            RegisterAppStart<TipViewModel>();
+        }
+    }
+}
---
+++ TipCalc.Core/ViewModels/TipViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.ViewModels;
+using TipCalc.Core.Services;
+
+namespace TipCalc.Core.ViewModels
+{
+    class TipViewModel : MvxViewModel
+    {
+        readonly ICalculationService calculationService;
+
+        private double subTotal;
+        private int generosity;
+        private double tip;
+
+        public double SubTotal
+        {
+            get => subTotal;
+            set
+            {
+                subTotal = value;
+                RaisePropertyChanged(() => SubTotal);
+
+                Recalculate();
+            }
+        }
+
+        public int Generosity
+        {
+            get => generosity;
+            set
+            {
+                generosity = value;
+                RaisePropertyChanged(() => Generosity);
+
+                Recalculate();
+            }
+        }
+
+        public double Tip
+        {
+            get => tip;
+            set
+            {
+                tip = value;
+                RaisePropertyChanged(() => Tip);
+            }
+        }
+
+        public TipViewModel(ICalculationService calculationService)
+        {
+            this.calculationService = calculationService;
+        }
+
+        public override async Task Initialize()
+        {
+            await base.Initialize();
+
+            subTotal = 100;
+            generosity = 10;
+
+            Recalculate();
+        }
+
+        private void Recalculate()
+        {
+            Tip = calculationService.TipAmount(SubTotal, Generosity);
+        }
+    }
+}
